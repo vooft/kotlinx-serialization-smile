abb3f8cd0d7ffd88ddbf7b00ec6b1cc40f2ddbe2
vooft <52610+vooft@users.noreply.github.com>
2024-05-09 15:01:46 +0400

    wip

--- kotlinx-serialization-smile-core/src/commonMain/kotlin/io/github/vooft/kotlinsmile/smile/SmileKeyToken.kt
+++ kotlinx-serialization-smile-core/src/commonMain/kotlin/io/github/vooft/kotlinsmile/smile/SmileKeyToken.kt
@@ -2,12 +2,11 @@ package io.github.vooft.kotlinsmile.smile
 
 sealed interface SmileKeyToken {
     val range: IntRange
-    val offset: Byte
+    val offset: Byte get() = (range.first - 1).toByte()
 }
 
 data object ShortAsciiName : SmileKeyToken {
     override val range = 0x80..0xBF
-    override val offset = (0x80 - 1).toByte()
 }
 
 
