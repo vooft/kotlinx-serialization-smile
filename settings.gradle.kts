3f4ef111a728e6967eff33b88bfffa187916ed8d
vooft <52610+vooft@users.noreply.github.com>
2024-05-14 09:09:05 +0400

    Minor benchmark changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ rootProject.name = "kotlinx-serialization-smile"
 
 include("kotlinx-serialization-smile-core")
 include("kotlinx-serialization-smile-test")
-include("kotlin-serialization-smile-benchmark")
+include("kotlinx-serialization-smile-benchmark")
